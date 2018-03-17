7e81d007e5f532ab7290d6fa47ea7cb9c72cd52c
sam-liem <8178799+sam-liem@users.noreply.github.com>
2018-03-17 12:00:25 +0000

    initial commit

---
+++ PocketMonster/PocketMonster/ViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ViewController.swift
+//  PocketMonster
+//
+//  Created by Dulio Denis on 11/5/15.
+//  Copyright © 2015 Dulio Denis. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var monsterImage: MonsterImage!
+    @IBOutlet weak var foodImage: DragImage!
+    @IBOutlet weak var heartImage: DragImage!
+    
+    @IBOutlet weak var penalty1Image: UIImageView!
+    @IBOutlet weak var penalty2Image: UIImageView!
+    @IBOutlet weak var penalty3Image: UIImageView!
+    
+    let DIM_ALPHA: CGFloat  = 0.2
+    let OPAQUE: CGFloat     = 1.0
+    let MAX_PENALTIES       = 3
+    var penalties           = 0
+    
+    var timer: NSTimer!
+    
+    var monsterHappy = false
+    var currentItem: UInt32 = 0
+    
+    var musicPlayer: AVAudioPlayer!
+    var sfxBite:     AVAudioPlayer!
+    var sfxHeart:    AVAudioPlayer!
+    var sfxDeath:    AVAudioPlayer!
+    var sfxSkull:    AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        foodImage.dropTarget = monsterImage
+        heartImage.dropTarget = monsterImage
+        
+        penalty1Image.alpha = DIM_ALPHA
+        penalty2Image.alpha = DIM_ALPHA
+        penalty3Image.alpha = DIM_ALPHA
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter:", name: "onTargetDropped", object: nil)
+        
+        do {
+            try musicPlayer = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("cave-music", ofType: "mp3")!))
+            try sfxBite = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bite", ofType: "wav")!))
+            try sfxHeart = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("heart", ofType: "wav")!))
+            try sfxDeath = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("death", ofType: "wav")!))
+            try sfxSkull = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("skull", ofType: "wav")!))
+            
+            musicPlayer.prepareToPlay()
+            musicPlayer.play()
+            
+            sfxBite.prepareToPlay()
+            sfxHeart.prepareToPlay()
+            sfxDeath.prepareToPlay()
+            sfxSkull.prepareToPlay()
+            
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
+        
+        startTimer()
+    }
+    
+    func itemDroppedOnCharacter(notification: AnyObject) {
+        // reset the timer when an item is dropped on our character
+        monsterHappy = true
+        startTimer()
+        
+        foodImage.alpha = DIM_ALPHA
+        foodImage.userInteractionEnabled = false
+        heartImage.alpha = DIM_ALPHA
+        heartImage.userInteractionEnabled = false
+        
+        if currentItem == 0 {
+            sfxHeart.play()
+        } else {
+            sfxBite.play()
+        }
+    }
+    
+    func startTimer() {
+        if timer != nil {
+            timer.invalidate()
+        }
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "changeGameState", userInfo: nil, repeats: true)
+    }
+    
+    func changeGameState() {
+        if !monsterHappy {
+            penalties++
+            
+            sfxSkull.play()
+            
+            if penalties == 1 {
+                penalty1Image.alpha = OPAQUE
+                penalty2Image.alpha = DIM_ALPHA
+            } else if penalties == 2 {
+                penalty2Image.alpha = OPAQUE
+                penalty3Image.alpha = DIM_ALPHA
+            } else if penalties == 3 {
+                penalty3Image.alpha = OPAQUE
+            } else {
+                penalty1Image.alpha = DIM_ALPHA
+                penalty2Image.alpha = DIM_ALPHA
+                penalty3Image.alpha = DIM_ALPHA
+            }
+            
+            if penalties >= MAX_PENALTIES {
+                gameOver()
+            }
+        }
+        
+        let rand = arc4random_uniform(2)
+        if rand == 0 {
+            foodImage.alpha = DIM_ALPHA
+            foodImage.userInteractionEnabled = false
+            
+            heartImage.alpha = OPAQUE
+            heartImage.userInteractionEnabled = true
+        } else {
+            foodImage.alpha = OPAQUE
+            foodImage.userInteractionEnabled = true
+            
+            heartImage.alpha = DIM_ALPHA
+            heartImage.userInteractionEnabled = false
+        }
+        
+        currentItem = rand
+        monsterHappy = false
+    }
+    
+    func gameOver() {
+        timer.invalidate()
+        monsterImage.playDeathAnimation()
+        sfxDeath.play()
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# diayeeties
+# Pocket Monster
+An iOS 9 [tamagotchi](https://en.wikipedia.org/wiki/Tamagotchi) virtual pet monster written in Swift 2. 
+
+![](https://github.com/duliodenis/pocketmonster/blob/master/art/pocketmonster2.gif)
+
+### Licensing
+Pocket Monster is licensed under [the MIT License](https://github.com/duliodenis/pocketmonster/blob/master/LICENSE).
+
+### Support or Contact
+Visit [ddApps.co](http://ddapps.co) to see more.
